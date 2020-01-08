d585c6d9f7635bd21e1071282e8106b2b2c7fa29
chrpruckner <christiane.pruckner@gmail.com>
2020-01-08 12:58:03 +0100

    cut description works now

--- src/App.js
+++ src/App.js
@@ -27,22 +27,45 @@ const Lists = (props) => {
   return <div className="row my-4 beer-list">{listItems}</div>;
 };
 
+const cutDescription = (str) => {
+  const maxLength = 120;
+  if (str.length > maxLength) {
+    let cutString = str.substr(0, maxLength);
+    const lastSpaceInString = cutString.lastIndexOf(" ");
+    const limit = cutString.length > lastSpaceInString ? lastSpaceInString : cutString.length;
+    cutString = cutString.substr(0, limit);
+    return (cutString + " ...");
+  }
+  else {
+    return str;
+  }
+}
+
 const ListItem = (props) => {
     const { id, name, description, image_url } = props;
+    const newDescription = cutDescription(description);
+    /* const maxLength = 120;
+    let cutString = description.length > maxLength && description.substring(0, maxLength);
+    const lastSpace = cutString.lastIndexOf(" ");
+    cutString = cutString.substring(0, Math.min(description.length, lastSpace));
+    const tail = " ..." */
+    //console.log(typeof(lastSpace));
     return ( 
       <div className="card-wrapper col-md-4 p-2 d-flex align-items-stretch">
-      {console.log("Props IDS", typeof(props.ids))}
-      {console.log("asdfa", typeof(props.id))}
+      {/*console.log("Props IDS", typeof(props.ids))*/}
+      {/*console.log("asdfa", typeof(props.id))*/}
         <div className="card mb-3 p-3">
           <div className="row">
-            <div className="col-md-3">
+            <div className="col-md-2 d-flex align-items-center">
               {image_url != null && <img src={image_url} className="card-img" alt="beer" />}
             </div>
-            <div className="col-md-8 d-flex flex-column align-itmes-stretch">
+            <div className="col-md-9 d-flex flex-column align-itmes-stretch">
               <div className="card-body text-left pt-0">
-    <p className="card-text">{props.ids.hasOwnProperty(id) ? <FaHeart onClick={() => props.setFav(id)} /> : <FaRegHeart onClick={() => props.setFav(id)} /> }</p>
+                <p className="card-text justify-content-end fav-icon">
+                  {props.ids.hasOwnProperty(id) ? <FaHeart onClick={() => props.setFav(id)} /> : <FaRegHeart onClick={() => props.setFav(id)} /> }
+                </p>
                 <h5 className="card-title">{name}</h5>
-                <p className="card-text">{description}</p>
+                <p className="card-text">{newDescription}</p>
               </div>
             </div>
           </div>
@@ -50,8 +73,6 @@ const ListItem = (props) => {
       </div>
     );
   };
-
-
 
 class App extends React.Component {
   constructor(props) {
@@ -169,7 +190,7 @@ class App extends React.Component {
     return (
       <Router>
         <div>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+          <nav className="navbar navbar-expand-lg navbar-light justify-content-between">
             <span className="navbar-brand">We Love Beer</span>
             <ul className="nav">
               <li className="nav-item p-2">
@@ -185,7 +206,9 @@ class App extends React.Component {
               renders the first one that matches the current URL. */}
           <Switch>
             <Route path="/favourites">
-              {this.state.fav_beers && <ListContainer beers1={this.state.fav_beers} setFav={this.setFav} ids={this.state.fav_ids} />}
+              {this.state.fav_beers && <div className="App">
+                <ListContainer beers1={this.state.fav_beers} setFav={this.setFav} ids={this.state.fav_ids} />
+                </div>}
             </Route>
             <Route path="/">
               <div className="App">
